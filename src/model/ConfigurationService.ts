21a98bbde294fe33b9a9c09d851917d71d4cdba0
seevrus <tiller2004@gmail.com>
2022-08-04 21:33:38 +0200

    Added default configuration

--- src/model/ConfigurationService.ts
+++ src/model/ConfigurationService.ts
@@ -1,15 +1,22 @@
 import { ApiConfigurationResponseT, ApiConfigurationT } from '../types/api-configuration';
 import { jsonFetch } from './utils';
 
+export const defaultConfiguration = {
+  baseUrl: 'https://image.tmdb.org/t/p/',
+  size: 'w185',
+};
+
 export class ConfigurationService {
   static async getConfiguration(): Promise<ApiConfigurationT> {
     return jsonFetch<ApiConfigurationResponseT>('https://api.themoviedb.org/3/configuration', {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
       },
-    }).then(response => ({
-      baseUrl: response.images.secure_base_url ?? response.images.base_url,
-      size: 'w185',
-    }));
+    })
+      .then(response => ({
+        baseUrl: response.images.secure_base_url ?? response.images.base_url,
+        size: 'w185',
+      }))
+      .catch(() => defaultConfiguration);
   }
 }
